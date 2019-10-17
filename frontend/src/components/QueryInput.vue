cf8ab60cebd31edb7d06891bfa96ee41aeec2bde
Ryan Tao <ryantao403@gmail.com>
2019-10-17 15:20:50 -0500

    button

--- frontend/src/components/QueryInput.vue
+++ frontend/src/components/QueryInput.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="query-input">
         <el-input v-model="query" placeholder="Enter the query" size='medium'></el-input>
+        <el-button type="primary" @click="search">Search</el-button>
     </div>    
 </template>
 
 <script>
 export default {
-    name: 'QueryInput'
+    name: 'QueryInput',
+    data() {
+        return {
+            query: ''
+        }        
+    },
+    methods: {
+        search() {
+            alert(this.query)
+        }
+    }
 }
 </script>
 
