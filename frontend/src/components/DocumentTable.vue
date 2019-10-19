845f3acd5301261329642b0026e320687ab8eb07
Ryan Tao <ryantao403@gmail.com>
2019-10-19 10:28:47 -0500

    Add link

--- frontend/src/components/DocumentTable.vue
+++ frontend/src/components/DocumentTable.vue
@@ -5,7 +5,12 @@
             style="width:80%"
             border
             empty-text="No data">
-            <el-table-column prop="title" label="Title" width="180"></el-table-column>
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="Title" width="180">
+                <template slot-scope="scope">
+                    <div><a :href="getAclLink(scope.row)">{{ scope.row.title }}</a></div>
+                </template>
+            </el-table-column>
             <el-table-column prop="abstract" label="Abstract"></el-table-column>
             <el-table-column
                 fixed="right"
@@ -23,6 +28,17 @@
 <script>
 export default {
     methods: {
+        getAclLink(row) {            
+            if(!row || !row.path) {
+                return ''
+            }
+            // sample: P01-1020.tei.xml, where P-01-1020 is the ACL ID
+            let dot = row.path.indexOf('.')
+            if(dot >= 0) {
+                return "https://www.aclweb.org/anthology/" + row.path.substring(0, dot) + ".pdf"
+            }
+            return ""
+        },
         yesClicked(row) {
             console.log('relevant' + row.title)
             // this.$store.dispatch('markRelevance', {
