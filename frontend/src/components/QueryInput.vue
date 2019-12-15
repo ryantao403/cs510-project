ab9ec86ff5c091c8f354d5ac87116248e95e7852
Kechen Lu <kechenl3@illinois.edu>
2019-12-14 20:32:15 -0600

    update
    Signed-off-by: Kechen Lu <kechenl3@illinois.edu>

--- frontend/src/components/QueryInput.vue
+++ frontend/src/components/QueryInput.vue
@@ -4,7 +4,7 @@
         <el-col :span="15">
           <el-autocomplete class="inline-input" v-model="query" placeholder="Enter the query" @keyup.enter.native="search" :fetch-suggestions="suggests" :trigger-on-focus="false" @select="change">
           <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
-        </el-autocomplete></el-col>
+	  </el-autocomplete></el-col>
         <el-col :span="9"><Topics /></el-col>
       </el-row>     
     </div>
@@ -49,5 +49,8 @@
   min-width: 1000px;
   display: inline-block;
   margin-bottom: 50px;
+}
+.inline-input {
+  width: 100%;
 }
 </style>
