bdccf4c4f3cdea09d0f8964a6e3c591abf575af8
xinranw5 <xinranw5@illinois.edu>
2019-12-15 19:49:47 -0600

    Router

---
+++ frontend/src/components/Page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+  	<h1>Here!</h1>
+  	<div>doc: {{ $route.params.id }}</div>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Page',
+
+  methods: {
+       
+    },
+}
+
+
+</script>
